15223210c139a8e8a07165449671fa8139c8688d
Ysaias Alvarez <yalvarezc@outlook.com>
2019-04-08 11:22:36 -0400

    Add project files.

---
+++ KiraHome/Controllers/AreaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiraHome.Controllers
+{
+    public class AreaController : Controller
+    {
+        public IActionResult Sala()
+        {
+            return View();
+        }
+
+        public IActionResult Cocina()
+        {
+            return View();
+        }
+
+        public IActionResult Habitacion()
+        {
+            return View();
+        }
+
+        public IActionResult Banio()
+        {
+            return View();
+        }
+    }
+}
---
+++ KiraHome/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KiraHome.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
