2c316bfec20e61b6f35891aef4a5f5769d26cc9c
Ysaias Alvarez <yalvarezc@outlook.com>
2019-04-17 15:11:29 -0400

    auth0 v1

---
+++ KiraHome/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiraHome.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });            
+        }
+    }
+}
--- KiraHome/Startup.cs
+++ KiraHome/Startup.cs
@@ -13,6 +13,15 @@ using Microsoft.EntityFrameworkCore;
 using KiraHome.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace KiraHome
 {
@@ -28,6 +37,8 @@ namespace KiraHome
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string host = "http://localhost:61991";
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -38,11 +49,78 @@ namespace KiraHome
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            /********************************/
+            
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; //CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme; //CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+                
+                // of course you also need to register that scheme, e.g. using
+                //options.AddScheme<MySchemeHandler>("scheme name", "scheme display name");
+
+            }).AddJwtBearer(o =>
+            {
+                // my API name as defined in Config.cs - new ApiResource... or in DB ApiResources table
+                o.Audience = Configuration["Settings:Authentication:ApiName"];
+                // URL of Auth server(API and Auth are separate projects/applications),
+                // so for local testing this is http://localhost:5000 if you followed ID4 tutorials
+                o.Authority = Configuration["Settings:Authentication:Authority"];
+                o.RequireHttpsMetadata = false;
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    // Scopes supported by API as defined in Config.cs - new ApiResource... or in DB ApiScopes table
+                    ValidAudiences = new List<string>() {
+                        "api.read",
+                        "api.write"
+                    },
+                    ValidateIssuer = true
+                };
+            })
+            .AddCookie()
+            .AddOAuth("KiraNodeServer", options =>
+            {
+                options.ClientId = Configuration["KiraServer:ClientId"];
+                options.ClientSecret = Configuration["KiraServer:ClientSecret"];
+                options.CallbackPath = new PathString("/Home");
+
+                options.AuthorizationEndpoint = host + "/api/Users/Authenticate";
+                options.TokenEndpoint = host + "/api/Users/Authenticate";
+                options.UserInformationEndpoint = host + "/api/Users/";
+
+                options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                options.ClaimActions.MapJsonKey(ClaimTypes.Name, "firstname");
+
+                options.Events = new OAuthEvents
+                {
+                    OnCreatingTicket = async context =>
+                    {
+                        var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                        var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                        response.EnsureSuccessStatusCode();
+
+                        var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                        context.RunClaimActions(user);
+
+                    }
+                };
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,7 +138,7 @@ namespace KiraHome
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
